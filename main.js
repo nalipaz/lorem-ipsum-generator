f0981557269644e7f8a58d06a572ed1d4e191c72
Nicholas Alipaz <nalipaz@labiomed.org>
2016-08-15 12:29:15 -0700

    Update app id

--- main.js
+++ main.js
@@ -181,4 +181,5 @@ function insertAtCursor(value) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
 		chrome.tabs.sendMessage(tabs[0].id, {lipsum: value}, function(response){});
 	});
+	chrome.runtime.sendMessage('dkgmhminaokkejenbkkjaeeimddfmbhb', value);
 }
